5170fb8c52584c1248977c1bfe8a4e9eb2cc35d6
RIDCorix <ridcorix@gmail.com>
2025-07-05 14:15:21 +0800

    [fix] build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/RIDCorix',
+  assetPrefix: '/RIDCorix/',
   images: {
     unoptimized: true
   }
